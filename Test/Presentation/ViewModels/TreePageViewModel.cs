1091108ead8b03b4ab420bb16e7965781cd0eb77
Andrés Zapata <maurozapata17@gmail.com>
2023-10-13 22:50:01 -0500

    Implemented TreeView using ViewModel

--- Test/Presentation/ViewModels/TreePageViewModel.cs
+++ Test/Presentation/ViewModels/TreePageViewModel.cs
@@ -1,12 +1,27 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Presentation.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Presentation.ViewModels
 {
     public class TreePageViewModel : ObservableObject
     {
+        public ICollection<TreeItem> Items { get; set; }
         public TreePageViewModel()
         {
-            
+            Items = new ObservableCollection<TreeItem>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                var folders = new TreeItem() { Name = $"Folder {i}" };
+                for (int j = 1; j <= 20; j++)
+                {
+                    var item = new TreeItem() { Name = $"Item {j}" , Icon = "Assets/StoreLogo.png" };
+                    folders.Children.Add(item);
+                }
+                Items.Add(folders);
+            }
         }
     }
 }
--- Test/Presentation/Views/TreePage.xaml.cs
+++ Test/Presentation/Views/TreePage.xaml.cs
@@ -1,13 +1,12 @@
 ﻿using Windows.UI.Xaml.Controls;
 
-
 namespace Presentation.Views
 {
     public sealed partial class TreePage : Page
     {
         public TreePage()
         {
-            InitializeComponent();
+            InitializeComponent();           
         }
     }
 }
