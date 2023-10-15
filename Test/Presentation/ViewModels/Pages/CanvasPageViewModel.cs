2ff340670f3fabffa2614728a1117217a0922131
Andrés Zapata <maurozapata17@gmail.com>
2023-10-15 01:29:56 -0500

    Implemented canvas using code behind

--- Test/Presentation/ViewModels/Pages/CanvasPageViewModel.cs
+++ Test/Presentation/ViewModels/Pages/CanvasPageViewModel.cs
@@ -1,17 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Presentation.ViewModels
 {
-    public class CanvasPageViewModel : ObservableObject
+    public partial class CanvasPageViewModel : ObservableObject
     {
         public CanvasPageViewModel()
         {
-
         }
     }
 }
